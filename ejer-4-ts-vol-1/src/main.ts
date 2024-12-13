58430a3a8a05ea95b573553d31ac133dedbec6c2
Iván Carrillo Vilar <ivancarrillo2467@gmail.com>
2024-12-13 20:05:43 +0100

    remake ejer4 as responsive nav with hamburger menu

--- ejer-4-ts-vol-1/src/main.ts
+++ ejer-4-ts-vol-1/src/main.ts
@@ -1,1 +1,5 @@
+const hamburgerMenu = document.querySelector(".hamburger-menu") as HTMLInputElement
+const links = document.querySelector(".links") as HTMLDivElement
+
+hamburgerMenu.addEventListener("click", () => links.classList.toggle("mobile-hidden"))
 
